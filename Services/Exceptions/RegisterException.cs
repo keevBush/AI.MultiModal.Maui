3b11583a00fc91e6f179336b42c1a374ef3b4c16
BUSH <keevbush@hotmail.com>
2025-08-21 16:08:15 +0200

    FIX

---
+++ Services/Exceptions/RegisterException.cs
@@ -0,0 +1,9 @@
+﻿namespace Exceptions
+{
+    public class RegisterException : Exception
+    {
+        public RegisterException(string message, string code) : base($"{code}:{message}")
+        {
+        }
+    }
+}
